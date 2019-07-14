3371b42e59f6d0a9a8752b2703241009bc463787
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/OfficeManager.php
+++ src/Managers/OfficeManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Office newEntity()
+ * @method \Amethyst\Schemas\OfficeSchema getSchema()
+ * @method \Amethyst\Repositories\OfficeRepository getRepository()
+ * @method \Amethyst\Serializers\OfficeSerializer getSerializer()
+ * @method \Amethyst\Validators\OfficeValidator getValidator()
+ * @method \Amethyst\Authorizers\OfficeAuthorizer getAuthorizer()
+ */
 class OfficeManager extends Manager
 {
     use ConfigurableManager;
